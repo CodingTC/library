c191ca76015a64f9bc3bee6343dd1e9eaa13a13b
TDC <trevordcooley@gmail.com>
2025-12-19 14:06:17 -0500

    Add display library function to update DOM

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 const myLibrary = [];
 
-
 function Book(title, author, numPages, isRead)
 {
     if(!new.target)
@@ -24,7 +23,35 @@ function addBookToLibrary(title, author, numPages, isRead)
 
 
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("IT", "Stephen King", 1045, true);
 
 console.log(theHobbit.displayInfo());
 
+const libraryTable = document.querySelector(".my-library-body");
 
+function displayLibrary()
+{
+    for(const book of myLibrary)
+    {
+        let newRow = document.createElement("tr");
+        let title = document.createElement("td");
+        let author = document.createElement("td");
+        let numPages = document.createElement("td");
+        let isRead = document.createElement("td");
+        let id = document.createElement("td");
+        title.textContent = book.title;     
+        newRow.appendChild(title);
+        author.textContent = book.author;     
+        newRow.appendChild(author);
+        numPages.textContent = book.numPages;     
+        newRow.appendChild(numPages);
+        isRead.textContent = book.isRead;     
+        newRow.appendChild(isRead);
+        id.textContent = book.id;     
+        newRow.appendChild(id);
+        libraryTable.appendChild(newRow);
+    }
+}
+
+displayLibrary();
